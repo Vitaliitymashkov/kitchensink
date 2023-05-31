8e7792e419082181d004b0ff5b3cf59be49de208
VitaliiTymashkov <vitalii.tymashkov@gmail.com>
2023-05-31 09:47:44 +0200

    update test_var

--- cypress/e2e/0-experiments/env_examples.cy.js
+++ cypress/e2e/0-experiments/env_examples.cy.js
@@ -2,6 +2,7 @@
 
 const username = Cypress.env("username");
 const password = Cypress.env("password1");
+const test_var = Cypress.env("TEST_VAR");
 //set CYPRESS_password1=pass111
 describe("example to-do app", () => {
   beforeEach(() => {
@@ -22,6 +23,7 @@ describe("example to-do app", () => {
     cy.log("ENV_CY_USER = " + username);
     cy.log("ENV_CY_PASS1 = " + password);
 
+    expect(test_var, "test_var was set").to.be.a("string").and.not.be.empty;
     expect(username, "username was set").to.be.a("string").and.not.be.empty;
 
     if (typeof password !== "string" || !password) {
