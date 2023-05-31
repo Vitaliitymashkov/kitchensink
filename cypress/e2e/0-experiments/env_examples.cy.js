8f7a7b93a1763ebaeb46f0035c1761883189793e
VitaliiTymashkov <vitalii.tymashkov@gmail.com>
2023-05-31 10:35:28 +0200

    add var

--- cypress/e2e/0-experiments/env_examples.cy.js
+++ cypress/e2e/0-experiments/env_examples.cy.js
@@ -24,7 +24,7 @@ describe("example to-do app", () => {
     cy.log("ENV_CY_PASS1 = " + password);
 
     expect(test_var, "test_var was set").to.be.a("string").and.not.be.empty;
-    expect(username, "username was set").to.be.a("string").and.not.be.empty;
+    expect(username, "username was set ()" + test_var).to.be.a("string").and.not.be.empty;
 
     if (typeof password !== "string" || !password) {
       throw new Error("Missing password value, set using CYPRESS_password=...");
